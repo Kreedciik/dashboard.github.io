929384bccf220df6d6968712f6f32347659543e6
Ilfat <68077959+Kreedciik@users.noreply.github.com>
2021-07-19 09:54:23 +0500

    Add new fies
    html, js, css

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const badgeBtn = document.querySelectorAll('.reason'),
+        overlay = document.querySelector('.overlay'),
+        loader = document.querySelector('#preloader');
+
+        window.onload = function(){
+            preloader.classList.add('hide');
+        }
+        
+badgeBtn.forEach( btn => {
+    btn.addEventListener('click', e => {
+        e.preventDefault();
+        overlay.classList.add('active');
+        document.body.overflow = 'hidden';
+    });
+});
+
+overlay.addEventListener('click', e => {
+    console.log(e.target)
+    if(e.target.classList.contains('close-button') || e.target === overlay){
+        overlay.classList.remove('active');
+        document.body.overflow = '';
+    }
+});
